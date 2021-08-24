fd71a841662b5f00092fb802479a5ff1d2130fa4
Kevin Goble <goble.kevin@gmail.com>
2021-08-23 22:59:15 -0700

    Completes challenge 10

--- src/recursion.js
+++ src/recursion.js
@@ -147,6 +147,22 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 1) {
+    return true;
+  }
+  if (string.length === 0) {
+    return true;
+  }
+  string = string.toUpperCase();
+  if (string.includes(' ')) {
+    string = string.split(' ').join('');
+  }
+  if (string.charAt(0) === string.charAt(string.length-1)) {
+    return palindrome(string.substring(1,string.length-1));
+  } else {
+    return false;
+  }
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
